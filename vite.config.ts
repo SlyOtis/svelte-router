20203c141b48ced63e736708552e2e6737329c7d
Martin Ottersland <ottin93@gmail.com>
2024-09-10 12:31:18 +0200

    Update Vite configuration to use Svelte plugin only

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import {resolve} from "path";
-import dts from "vite-plugin-dts";
-import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    svelte({
-      compilerOptions: {
-        customElement: false,
-      },
-    }),
-    dts({
-      include: ["./src/lib"],
-      insertTypesEntry: true,
-      rollupTypes: true,
-    }),
+    svelte()
   ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: pkg.name,
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'umd'],
-    },
-    rollupOptions: {
-      external: ['svelte'],
-      output: {
-        globals: {
-          svelte: 'Svelte',
-        },
-      },
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 });
